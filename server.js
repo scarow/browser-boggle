7d23b8f2448a96aaa6a791c0fffafe3717336862
Samantha Carow <samantha.carow@gmail.com>
2016-01-27 22:30:45 -0800

    deploy

--- server.js
+++ server.js
@@ -12,21 +12,8 @@ if (isProd){
       res.sendFile('index.html', {
         root: static_path
       });
-    }).listen(process.env.PORT || 8080, function (err) {
+    }).listen(process.env.PORT, function (err) {
       if (err) { console.log(err) };
       console.log('Listening at localhost:8080');
     });
 }
-
-// if (isDevelopment) {
-//   var config = require('./webpack.config');
-//   var WebpackDevServer = require('webpack-dev-server');
-
-//   new WebpackDevServer(webpack(config), {
-//     publicPath: config.output.publicPath,
-//     hot: true
-//   }).listen(3000, 'localhost', function (err, result) {
-//     if (err) { console.log(err) }
-//     console.log('Listening at localhost:3000');
-//   });
-// }
